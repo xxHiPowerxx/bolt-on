120969e1a81d79e1a499c5d7035e51d12ee98183
Ricky Adams <ra@main.local>
2020-02-25 13:10:19 -0800

    added collapseOnHover function for nav

--- src/js/bolt-on.js
+++ src/js/bolt-on.js
@@ -1,1 +1,24 @@
 // Write JS here
+jQuery(document).ready(function($) {
+	function collapseOnHover() {
+		$('.has-dropdown').each(function() {
+			var collapse = $(this).children('.collapse'),
+				className = 'active';
+			$(this).hover(
+				function() {
+					$(this).addClass(className);
+					collapse.collapse('show');
+				},
+				function() {
+					$(this).removeClass(className);
+					collapse.collapse('hide');
+				}
+			);
+		});
+	}
+
+	function readyFuncs() {
+		collapseOnHover();
+	}
+	readyFuncs();
+});
