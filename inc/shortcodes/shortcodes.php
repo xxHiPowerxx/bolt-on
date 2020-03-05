2737ad6fe97db2c8a2916783cbb471a9ca704d74
Ricky Adams <ra@main.local>
2020-03-05 09:29:38 -0800

    added year shortcode

--- inc/shortcodes/shortcodes.php
+++ inc/shortcodes/shortcodes.php
@@ -6,24 +6,33 @@
  * @package bolt-on
  */
 
+/**
+ * Year shortcode.
+ */
+function get_year_func() {
+	$year = date( 'Y' );
+	return $year;
+}
+add_shortcode( 'year', 'get_year_func' );
+
+/**
+ * Recent Posts shortcode.
+ */
 function recent_posts_function( $atts ) {
 	$max_posts = get_option( 'posts_per_page' );
 	extract( shortcode_atts( array(
 		'posts' => $max_posts,
 	), $atts ) );
-	// $content = '<ul>';
-		query_posts( array('orderby' => 'date', 'order' => 'DESC' , 'showposts' => $posts) );
-		// $content = ob_start();
-		ob_start();
-		if ( have_posts() ) :
-			
-			while ( have_posts() )
-			: the_post();
-			get_template_part( 'template-parts/content-archive', get_post_type() );
-			
-			endwhile;
-		endif;
-		// $content .= '</ul>';
+	query_posts( array('orderby' => 'date', 'order' => 'DESC' , 'showposts' => $posts) );
+	ob_start();
+	if ( have_posts() ) :
+		
+		while ( have_posts() )
+		: the_post();
+		get_template_part( 'template-parts/content-archive', get_post_type() );
+		
+		endwhile;
+	endif;
 
 	wp_reset_query();
 	return ob_get_clean();
