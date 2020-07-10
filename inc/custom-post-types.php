5b17373789fbef6abaae46247f94b2393c8e024d
Ricky Adams <ra@main.local>
2020-07-10 10:32:46 -0700

    removed createCustomPostType() Utility function, now configure args manually

--- inc/custom-post-types.php
+++ inc/custom-post-types.php
@@ -12,19 +12,13 @@
  */
 function bolt_on_custom_post_types() {
 	/**
-	 * Utility Function that creates a single post type.
-	 * This way we don't have to rewrite the code more than once,
-	 * but simply pass our arguments into the function.
+	 * Utility Function that Creates the Post Label programmaitcally.
 	 */
-	function create_custom_post_type (
+	function create_post_labels(
 		$post_singular,
 		$post_plural      = null,
-		$post_description = null,
-		$hierarchical     = false,
-		$icon             = 'admin-post',
-		$rewrite          = null
+		$post_description = null
 	) {
-		// Defaults
 		$post_plural      = $post_plural === null ?
 												$post_singular . 's' :
 												$post_plural;
@@ -32,89 +26,161 @@ function bolt_on_custom_post_types() {
 												$post_plural :
 												$post_description;
 		$post_handle      = str_replace( ' ','-', strtolower( $post_plural ) );
-		if ( $rewrite === null ) :
-			$rewrite = array(
-				'slug       '=> $post_handle,
-				'with_front' => true,
-			);
-		endif;
 
-		$menu_icon        = 'dashicons-' . $icon;
-		$theme            = 'bolt-on';
-		// Set UI labels for Custom Post Type
-		$post_labels = array(
-			'name'                => _x( $post_plural, 'Post Type General Name', $theme ),
-			'singular_name'       => _x( $post_singular, 'Post Type Singular Name', $theme ),
-			'menu_name'           => __( $post_plural, $theme ),
-			'parent_item_colon'   => __( 'Parent ' . $post_singular, $theme ),
-			'all_items'           => __( 'All ' . $post_plural, $theme ),
-			'view_item'           => __( 'View ' . $post_singular, $theme ),
-			'add_new_item'        => __( 'Add New ' . $post_singular, $theme ),
-			'add_new'             => __( 'Add New', $theme ),
-			'edit_item'           => __( 'Edit ' . $post_singular, $theme ),
-			'update_item'         => __( 'Update ' . $post_singular, $theme ),
-			'search_items'        => __( 'Search ' . $post_singular, $theme ),
-			'not_found'           => __( 'Not Found', $theme ),
-			'not_found_in_trash'  => __( 'Not found in Trash', $theme ),
+		$return_result    = array(
+			'post_singular' => $post_singular,
+			'post_plural' => $post_plural,
+			'post_description' => $post_description,
+			'post_handle' => $post_handle,
 		);
-		// Set other options for Custom Post Type
-		$args = array(
-			'label'               => __( $post_plural, $theme ),
-			'description'         => __( $post_description, $theme ),
-			'labels'              => $post_labels,
-			// Features this CPT supports in Post Editor
-			'supports'            => array(
-																'title',
-																'thumbnail',
-																'custom-fields',
-																'page-attributes',
-															),
-			// You can associate this CPT with a taxonomy or custom taxonomy. 
-			'taxonomies'          => array( 'practice-areas' ),
-			/* A hierarchical CPT is like Pages and can have
-			* Parent and child items. A non-hierarchical CPT
-			* is like Posts.
-			*/ 
-			'hierarchical'        => $hierarchical,
-			'public'              => true,
-			'show_ui'             => true,
-			'show_in_menu'        => true,
-			'show_in_nav_menus'   => true,
-			'show_in_rest'        => true,
-			'show_in_admin_bar'   => true,
-			'menu_position'       => 5,
-			'can_export'          => true,
-			'has_archive'         => true,
-			'exclude_from_search' => false,
-			'publicly_queryable'  => true,
-			'capability_type'     => 'post',
-			'show_in_rest'        => true,
-			'menu_icon'           => $menu_icon,
-			'rewrite'             => $rewrite,
-		);
-		// Registering your Custom Post Type
-		return register_post_type( $post_handle, $args );
+
+		return $return_result;
 	}
-	// Use our utility function to render different Custom Post Types.
-	create_custom_post_type( 
-		'Attorney',
+
+	/**
+	 * Create Attorneys Custom Post Type
+	 */
+	$post_singular    = 'Attorney';
+	$post_names      = create_post_labels(
+		$post_singular,
 		null,
-		'Attorneys and Staff',
-		false,
-		'businessman'
+		'Attorneys and Staff'
 	);
-	create_custom_post_type(
-		'Practice Area',
-		null,
-		null,
-		true,
-		'welcome-learn-more',
-		array(
+	$post_plural      = $post_names['post_plural'];
+	$post_description = $post_names['post_description'];
+	$post_handle      = $post_names['post_handle'];
+
+	$icon             = 'businessman';
+	$menu_icon        = 'dashicons-' . $icon;
+	$theme            = 'bolt-on';
+	// Set UI labels for Custom Post Type
+	$post_labels = array(
+		'name'                => _x( $post_plural, 'Post Type General Name', $theme ),
+		'singular_name'       => _x( $post_singular, 'Post Type Singular Name', $theme ),
+		'menu_name'           => __( $post_plural, $theme ),
+		'parent_item_colon'   => __( 'Parent ' . $post_singular, $theme ),
+		'all_items'           => __( 'All ' . $post_plural, $theme ),
+		'view_item'           => __( 'View ' . $post_singular, $theme ),
+		'add_new_item'        => __( 'Add New ' . $post_singular, $theme ),
+		'add_new'             => __( 'Add New', $theme ),
+		'edit_item'           => __( 'Edit ' . $post_singular, $theme ),
+		'update_item'         => __( 'Update ' . $post_singular, $theme ),
+		'search_items'        => __( 'Search ' . $post_singular, $theme ),
+		'not_found'           => __( 'Not Found', $theme ),
+		'not_found_in_trash'  => __( 'Not found in Trash', $theme ),
+	);
+	// Set other options for Custom Post Type
+	$args = array(
+		'label'               => __( $post_plural, $theme ),
+		'description'         => __( $post_description, $theme ),
+		'labels'              => $post_labels,
+		// Features this CPT supports in Post Editor
+		'supports'            => array(
+															'title',
+															'thumbnail',
+															'custom-fields',
+															'page-attributes',
+														),
+		// You can associate this CPT with a taxonomy or custom taxonomy. 
+		// 'taxonomies'          => array( 'practice-areas' ),
+		/* A hierarchical CPT is like Pages and can have
+		* Parent and child items. A non-hierarchical CPT
+		* is like Posts.
+		*/ 
+		'hierarchical'        => false,
+		'public'              => true,
+		'show_ui'             => true,
+		'show_in_menu'        => true,
+		'show_in_nav_menus'   => true,
+		'show_in_rest'        => true,
+		'show_in_admin_bar'   => true,
+		'menu_position'       => 5,
+		'can_export'          => true,
+		'has_archive'         => true,
+		'exclude_from_search' => false,
+		'publicly_queryable'  => true,
+		'capability_type'     => 'post',
+		'show_in_rest'        => true,
+		'menu_icon'           => $menu_icon,
+		'rewrite'             => array(
+			'slug       '=> $post_handle,
+			'with_front' => true,
+		),
+	);
+	// Registering your Custom Post Type
+	register_post_type( $post_handle, $args );
+
+	/**
+	 * Create Attorneys Custom Post Type.
+	 */
+
+	$post_singular    = 'Practice Area';
+	$post_names      = create_post_labels( $post_singular );
+	$post_plural      = $post_names['post_plural'];
+	$post_description = $post_names['post_description'];
+	$post_handle      = $post_names['post_handle'];
+
+	$icon             = 'welcome-learn-more';
+	$menu_icon        = 'dashicons-' . $icon;
+	$theme            = 'bolt-on';
+	// Set UI labels for Custom Post Type
+	$post_labels = array(
+		'name'                => _x( $post_plural, 'Post Type General Name', $theme ),
+		'singular_name'       => _x( $post_singular, 'Post Type Singular Name', $theme ),
+		'menu_name'           => __( $post_plural, $theme ),
+		'parent_item_colon'   => __( 'Parent ' . $post_singular, $theme ),
+		'all_items'           => __( 'All ' . $post_plural, $theme ),
+		'view_item'           => __( 'View ' . $post_singular, $theme ),
+		'add_new_item'        => __( 'Add New ' . $post_singular, $theme ),
+		'add_new'             => __( 'Add New', $theme ),
+		'edit_item'           => __( 'Edit ' . $post_singular, $theme ),
+		'update_item'         => __( 'Update ' . $post_singular, $theme ),
+		'search_items'        => __( 'Search ' . $post_singular, $theme ),
+		'not_found'           => __( 'Not Found', $theme ),
+		'not_found_in_trash'  => __( 'Not found in Trash', $theme ),
+	);
+	// Set other options for Custom Post Type
+	$args = array(
+		'label'               => __( $post_plural, $theme ),
+		'description'         => __( $post_description, $theme ),
+		'labels'              => $post_labels,
+		// Features this CPT supports in Post Editor
+		'supports'            => array(
+															'title',
+															'editor',
+															'thumbnail',
+															'custom-fields',
+															'page-attributes',
+														),
+		// You can associate this CPT with a taxonomy or custom taxonomy. 
+		// 'taxonomies'          => array( 'practice-areas' ),
+		/* A hierarchical CPT is like Pages and can have
+		* Parent and child items. A non-hierarchical CPT
+		* is like Posts.
+		*/ 
+		'hierarchical'        => true,
+		'public'              => true,
+		'show_ui'             => true,
+		'show_in_menu'        => true,
+		'show_in_nav_menus'   => true,
+		'show_in_rest'        => true,
+		'show_in_admin_bar'   => true,
+		'menu_position'       => 5,
+		'can_export'          => true,
+		'has_archive'         => true,
+		'exclude_from_search' => false,
+		'publicly_queryable'  => true,
+		'capability_type'     => 'post',
+		'show_in_rest'        => true,
+		'menu_icon'           => 'businessman',
+		'rewrite'             => array(
 			'slug'       => '/',
 			'with_front' => false,
-		)
+		),
 	);
-	
+	// Registering your Custom Post Type
+	register_post_type( $post_handle, $args );
+
 }
 	 
 	/**
