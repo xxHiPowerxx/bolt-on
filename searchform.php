7a340f36d8bbcce42b4142b021e038f932dcf1d9
Ricky Adams <ra@main.local>
2020-09-22 13:56:45 -0700

    added .form-control class to input field

--- searchform.php
+++ searchform.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Template for displaying search forms in Twenty Eleven
- *
+ * Template for displaying search forms 
+ * 
  * @package WordPress
  * @subpackage Bolt-On
  */
@@ -12,7 +12,7 @@ $submit_id = esc_attr( 'submit-search-' . $instance );
 ?>
 <form method="get" class="search-form display-flex" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<label class="fa fa-search icon-search" for="<?php echo $search_id; ?>"></label>
-	<input id="<?php echo $search_id; ?>" type="search" class="input-search" name="s" title="Search" aria-label="Search" required>
+	<input id="<?php echo $search_id; ?>" type="search" class="input-search form-control" name="s" title="Search" aria-label="Search" required>
 	<button id="<?php echo $search_id; ?>" type="submit" class="btn-cta-outer stroke-border btn-submit" aria-label="Search Button" name="submit">
 		<span class="btn-cta btn-cta-inner stroke-border-inner">
 			<span class="btn-cta-text stroke-border-lvl-three">Search</span>
