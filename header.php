50d18eadc24d2b740b8cee3108ef7dd0e6959d40
Ricky Adams <ra@main.local>
2020-10-07 12:13:21 -0700

    added #show-header element that displays when header disappears

--- header.php
+++ header.php
@@ -34,12 +34,19 @@
 	$mobile_nav_breakpoint_attr = 'data-mobile-nav-breakpoint="' . $mobile_nav_breakpoint . '"';
 ?>
 <body <?php body_class('fixed-header hide-header' . ' ' . $body_class); ?> <?php echo $mobile_nav_breakpoint_attr; ?>>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'bolt-on' ); ?></a>
+	<div id="page" class="site">
+		<a class="skip-link screen-reader-text" href="#content" tabindex="1"><?php esc_html_e( 'Skip to Content', 'bolt-on' ); ?></a>
 
-	<header id="masthead" class="site-header sizeHeaderPad scrolledPastHeaderRef">
-		<?php include $template_directory . '/inc/header-top.php'; ?>
-		<?php include $template_directory . '/inc/header-nav.php'; ?>
-	</header><!-- #masthead -->
+		<header id="masthead" class="site-header sizeHeaderPad scrolledPastHeaderRef">
+			<?php include $template_directory . '/inc/header-top.php'; ?>
+			<?php include $template_directory . '/inc/header-nav.php'; ?>
+			<div id="show-header">
+				<a href="<?php echo esc_url( home_url( '/' ) ) ?>" rel="home" class="show-header-icon" tabindex="0">
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 67.64 34.73"><path d="M4.85,28.34C3.9,34.29,0,34.73,0,34.73H9.7S5.8,34.29,4.85,28.34Z" style="fill:#345073"/><polygon points="22.63 34.73 23.86 34.73 36.7 11.82 35.75 11.29 27.03 26.86 13.24 0 3.49 0 5.5 1.38 22.63 34.73" style="fill:#345073"/><path d="M67.64,34.73,49.81,0H40.06l2,1.37,8.66,16.87H43.78s7.77,2.65,11.91,9.67l3.5,6.82Z" style="fill:#345073"/><polygon points="45.14 27.05 31.25 0 21.5 0 23.51 1.38 40.63 34.73 45.14 27.05" style="fill:#ad976e"/></svg>
+				</a>
+				<i class="fa fas fa-chevron-down"></i>
+				<i class="fa fas fa-bars"></i>
+			</div>
+		</header><!-- #masthead -->
 
-	<div id="content" class="site-content sizeHeaderPadTar">
+		<div id="content" class="site-content sizeHeaderPadTar">
