f982663bd2bfc3e14e88a82bbd418eb51d698ca0
Ricky Adams <ra@main.local>
2020-10-20 11:14:54 -0700

    added tabindex to submit button in search form

--- searchform.php
+++ searchform.php
@@ -13,7 +13,7 @@ $submit_id = esc_attr( 'submit-search-' . $instance );
 <form method="get" class="search-form display-flex" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<label class="fa fa-search icon-search" for="<?php echo $search_id; ?>"></label>
 	<input id="<?php echo $search_id; ?>" type="search" class="input-search form-control" name="s" title="Search" aria-label="Search" required>
-	<button id="<?php echo $search_id; ?>" type="submit" class="btn-cta-outer stroke-border btn-submit" aria-label="Search Button" name="submit">
+	<button id="<?php echo $search_id; ?>" type="submit" class="btn-cta-outer stroke-border btn-submit" aria-label="Search Button" name="submit" tabindex="-1">
 		<span class="btn-cta btn-cta-inner stroke-border-inner">
 			<span class="btn-cta-text stroke-border-lvl-three">Search</span>
 		</span>
