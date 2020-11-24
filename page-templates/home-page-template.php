b963d7995e179d75b81e57df4c92134d3a4eb349
Ricky Adams <ra@main.local>
2020-11-24 09:29:14 -0800

    removed enqueue for bolt-on-js script

--- page-templates/home-page-template.php
+++ page-templates/home-page-template.php
@@ -9,8 +9,8 @@
 $home_page_template_css_path = '/assets/css/home-page-template.css';
 wp_enqueue_style( 'home-page-template-css', get_theme_file_uri( $home_page_template_css_path ), array( 'bolt-on-vendor-slick-css', 'bolt-on-css' ), filemtime( get_template_directory() . $home_page_template_css_path ), 'all' );
 
-$home_page_template_js_path = '/assets/js/bolt-on.js';
-wp_enqueue_script( 'bolt-on-js', get_theme_file_uri( $home_page_template_js_path ), array( 'jquery', 'bolt-on-vendor-bootstrap-js', 'bolt-on-vendor-slick-js' ), filemtime( get_template_directory() . $home_page_template_js_path ), false );
+// Enqueue Scripts
+wp_enqueue_script( 'bolt-on-vendor-slick-js' );
 
 // Add trans-header class to body.
 function add_home_page_template_class( $class ) {
